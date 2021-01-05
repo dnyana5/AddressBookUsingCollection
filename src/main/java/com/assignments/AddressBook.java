19a224d9a2b13638e19954250f1413bee02bafdd
Dnyaneshwari <dnyaneshwarigaikwad5@gmail.com>
2021-01-05 14:43:56 +0530

    Added:Methods to edit details

--- src/main/java/com/assignments/AddressBook.java
+++ src/main/java/com/assignments/AddressBook.java
@@ -6,5 +6,7 @@ public class AddressBook {
         AddressMethods address=new AddressMethods();
         address.Add();
         address.Show();
+        address.Edit();
+        address.Show();
     }
 }
